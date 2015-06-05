2be9ea0b65870e3fc57f5a36b0103e831d85aa92
Ben Sweeney <benjamin.sweeney.r@gmail.com>
2015-06-05 14:57:11 -0500

    Initial Commit

---
+++ gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'css/4392_styles.css' : 'sass/4392_styles.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	// grunt.registerTask('default',['watch']);
+	grunt.registerTask('derp',['watch']);
+
+}
